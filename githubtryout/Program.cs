3cea76a0469a84d8ae17e2bda062c3028a7eaf67
lancelot1926 <es.huluca@gmail.com>
2022-04-15 22:06:33 +0300

    first commit

---
+++ githubtryout/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace githubtryout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
